166a39c095e17e340dee9fec045e180a68f1b46e
Devmodjo <yvankamsu88@gmail.com>
2025-05-24 12:18:49 +0100

    creation du controller de suppression de session

--- backend/controllers/session.controller.js
+++ backend/controllers/session.controller.js
@@ -20,3 +20,21 @@ module.exports.setSession = async (req, res) => {
 
   res.json(session);
 };
+
+module.exports.deleteSession = async (req, res) => {
+  if (!req.body) {
+    res
+      .status(400)
+      .json({ message: "veuillez renseigner toutes les information" });
+    return;
+  }
+
+  const session = await sessionModel.findByIdAndDelete(req.params.id);
+
+  if (!session) {
+    res.status(404).json({ message: "Session non trouvée" });
+    return;
+  }
+
+  res.json({ message: "Session supprimée avec succès" });
+};
